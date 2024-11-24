57cc4cbec8ae4567edce74841cc5cd9d90e9e239
sparks042 <chinedufreedom10@gmail.com>
2024-11-24 22:33:16 +0100

    updating

---
+++ login.js
@@ -0,0 +1,27 @@
+// const userEmail = document.getElementById("email")
+// const userPassword = document.getElementById("password")
+// const logInButton = document.getElementById("login")
+// const emailErrorMessage = document.getElementById("email-error-message")
+// const passwordErrorMessage = document.getElementById("password-error-message")
+
+
+// logInButton.addEventListener("click", (e) =>
+//     {
+//         e.preventDefault();
+//         if(userEmail.value === ""){
+//             emailErrorMessage.textContent = "Email field cannot be left empty"
+//         }
+//         else if(!validateEmail(userEmail.value)){
+//             errorMessage.textContent = "Invalid email address"
+//         }
+//         else{
+//             errorMessage.textContent.style.display = "none"
+//         }
+//         if(userPassword.value === ""){
+//             passwordErrorMessage.textContent = "Password field cannot be left empty"
+//         }
+//         else{
+//             passwordErrorMessage.textContent.style.display = "none"
+//         }
+//     }
+// )
